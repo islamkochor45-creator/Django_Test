3e84d863ce69db5e798c4be4bc095558dd212896
islam <islamkochorbaev2@gmail.com>
2026-07-18 11:51:20 +0600

    fix: settings django-filter

--- DjangoInternetShops/settings.py
+++ DjangoInternetShops/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "django_filters",
+    "django_filter",
     "drf_spectacular",
     "jazzmin",
     "apps.users",
