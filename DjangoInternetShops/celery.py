fb2be18f187ebf41189122fa0243e080a1eee7a5
islam <islamkochorbaev2@gmail.com>
2026-07-20 16:48:32 +0600

    fix: API3

--- DjangoInternetShops/celery.py
+++ DjangoInternetShops/celery.py
@@ -1,11 +1,20 @@
 import os
 
+import django
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoInternetShops.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "DjangoInternetShops.settings",
+)
+
+django.setup()
 
 app = Celery("DjangoInternetShops")
 
-app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object(
+    "django.conf:settings",
+    namespace="CELERY",
+)
 
 app.autodiscover_tasks()
