3d3027e15f54726b327c8254ee61d8f5cf62f9c1
islam <islamkochorbaev2@gmail.com>
2026-07-21 15:39:31 +0600

    task

--- ShopAPI/tasks.py
+++ ShopAPI/tasks.py
@@ -6,8 +6,8 @@ from django.conf import settings
 @shared_task
 def send_email_task(email):
     send_mail(
-        subject="Подтверждение регистрации",
-        message="Добро пожаловать!",
+        subject="Подтверждение регистрации ",
+        message="Добро пожаловать! В сайт",
         from_email=settings.DEFAULT_FROM_EMAIL,
         recipient_list=[email],
         fail_silently=False,
