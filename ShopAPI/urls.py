16480c2d003731d200562fc8744977e37c36e22d
islam <islamkochorbaev2@gmail.com>
2026-07-21 15:29:37 +0600

    fix: views registr users task

--- ShopAPI/urls.py
+++ ShopAPI/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet
+from .views import UserViewSet, RegisterView
 
 router = DefaultRouter()
 router.register(r"users", UserViewSet, basename="user")
+router.register(r"users", RegisterView, basename="user")
 
 urlpatterns = [
     path("tasks/", include(router.urls)),
+    path("create_users", include(router.urls)),
 ]
