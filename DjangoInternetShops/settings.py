bc408d7efd49a2fff499ff1c56b1ce8239a0a1f4
islam <islamkochorbaev2@gmail.com>
2026-07-21 22:45:41 +0600

    fix: settings 3 version

--- DjangoInternetShops/settings.py
+++ DjangoInternetShops/settings.py
@@ -203,7 +203,20 @@ CSRF_TRUSTED_ORIGINS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.User"
-DEFAULT_FROM_EMAIL = "islamkochor45@gmail.com"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "islamkochor45@gmail.com"
+EMAIL_HOST_PASSWORD = "qwyp uhxl izbo rqgh"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+# DEFAULT_FROM_EMAIL = "islamkochor45@gmail.com"
 
 MEDIA_URL = "/media/"
 
