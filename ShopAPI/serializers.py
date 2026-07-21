7169e6224dbb52a64be3ebec71bfdc4c585b7079
islam <islamkochorbaev2@gmail.com>
2026-07-21 21:07:37 +0600

    fix: serializers

--- ShopAPI/serializers.py
+++ ShopAPI/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 
 
 class RegisterSerializer(serializers.Serializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            email=validated_data["email"], password=validated_data["password"]
+        )
