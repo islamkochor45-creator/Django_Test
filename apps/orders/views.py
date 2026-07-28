96fc9aeb6909a57527be5165d58151fe4247f52f
islam <islamkochorbaev2@gmail.com>
2026-07-28 16:05:31 +0600

    fix: settings veiws schema

--- apps/orders/views.py
+++ apps/orders/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from drf_spectacular.utils import extend_schema
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import IsAdminUser
@@ -10,6 +11,10 @@ from .models import Order
 from .serializers import OrderSerializer
 
 
+@extend_schema(
+    request=OrderSerializer,
+    responses={200: None},
+)
 class CreateOrderAPIView(APIView):
 
     permission_classes = [IsAuthenticated]
