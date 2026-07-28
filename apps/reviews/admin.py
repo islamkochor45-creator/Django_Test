16b7d38a8eea721beb636320bec9fa77c3ab7794
islam <islamkochorbaev2@gmail.com>
2026-07-28 16:49:39 +0600

    fix: admin.py in reviews

--- apps/reviews/admin.py
+++ apps/reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Review
+
 
 # Register your models here.
+@admin.register(Review)
+class ResviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "product",
+        "rating",
+        "text",
+        "created_at",
+        "updated_at",
+    )
