d65cb9ca6e1e5275a5693bbfb2ff54186853061c
islam <islamkochorbaev2@gmail.com>
2026-07-28 23:29:04 +0600

    urls.py update 2.0

--- apps/reviews/urls.py
+++ apps/reviews/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ProductReviewsAPIView, CreateReviewAPIView
 
 urlpatterns = [
-    path("product/<int:product_id>/", CreateReviewAPIView.as_view()),
-    path("create/", ProductReviewsAPIView.as_view()),
+    path("create/", CreateReviewAPIView.as_view()),
+    path("product/<int:product_id>/", ProductReviewsAPIView.as_view()),
 ]
